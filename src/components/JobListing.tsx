c90c5192b2c583e318fa486e5383b43590fb2475
Joe342wise <jnyarko342@gmail.com>
2025-11-21 15:30:52 +0000

    Add react-icons and react-router-dom dependencies; enhance JobListing component with expandable description feature

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,5 +1,6 @@
 // import React from 'react'
-
+import { useState } from "react"
+import { FaMapMarker } from "react-icons/fa"
 interface Company {
   name: string
   description: string
@@ -22,6 +23,15 @@ interface JobListingProps {
 }
 
 const JobListing = ({jobs}: JobListingProps) => {
+    const [showFullDescription, setShowFullDescription] = useState(false);
+
+    let description = jobs.description;
+
+    if (!showFullDescription) {
+        description = description.substring(0, 100) + '...';
+    }
+
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
                 <div className="p-4">
@@ -31,8 +41,15 @@ const JobListing = ({jobs}: JobListingProps) => {
                   </div>
     
                   <div className="mb-5">
-                   {jobs.description}
+                   {description}
                   </div>
+
+                  <button 
+                  type="button"
+                  onClick={() => setShowFullDescription((prevState) => !prevState)} 
+                  className="text-indigo-500 mb-5 hover:text-indigo-600"> 
+                    {showFullDescription ? "Less" : "More"} 
+                    </button>
     
                   <h3 className="text-indigo-500 mb-2"> {jobs.salary} / Year</h3>
     
@@ -40,7 +57,7 @@ const JobListing = ({jobs}: JobListingProps) => {
     
                   <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                      <i className="fa-solid fa-location-dot text-lg"></i>
+                      <FaMapMarker className="inline text-lg mb-1 mr-1" />
                       {jobs.location}
                     </div>
                     <a
