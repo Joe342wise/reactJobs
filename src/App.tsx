e6747257a2ff39b264f6a4f596f38ce1c3fb73ae
Joe342wise <jnyarko342@gmail.com>
2025-11-25 14:16:45 +0000

    Add AddJobPage component for job creation; integrate react-toastify for notifications
    - Introduced AddJobPage component to facilitate job creation with a form.
    - Implemented addJob function in App component to handle job submissions.
    - Enhanced JobPage component to include delete functionality with confirmation and notifications.
    - Integrated react-toastify for user feedback on job addition and deletion.
    - Updated MainLayout to include ToastContainer for displaying notifications.

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,60 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import JobPage from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 import jobLoader from './loaders/jobLoader';
 import NotFoundPage from './pages/NotFoundPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Route>
-)
-)
+
+type JobCompany = {
+  name: string;
+  description: string;
+  contactEmail: string;
+  contactPhone: string;
+};
+
+type JobFormData = {
+  title: string;
+  type: string;
+  location: string;
+  description: string;
+  salary: string;
+  company: JobCompany;
+};
 
 const App = () => {
+  // Add new Job
+  const addJob = async(newJob: JobFormData) => {
+    await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  // Delete Job
+  const deleteJob = async (id: string) => {
+    await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE'
+    });
+    return;
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route path='/add-job/' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+  
   return <RouterProvider router={router}/>
 }
 
