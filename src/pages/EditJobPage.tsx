ef5d389b08895337122d77927495416f22db684b
Joe342wise <jnyarko342@gmail.com>
2025-11-26 11:14:36 +0000

    Add Tailwind CSS configuration and update styles for job pages
    - Introduced tailwind.config.js to configure Tailwind CSS with custom font and grid settings.
    - Updated index.css to define a CSS variable for grid column layout.
    - Modified EditJobPage to change button text from "Add Job" to "Update Job" for clarity.
    - Adjusted JobPage to use a new grid template for responsive design.

--- src/pages/EditJobPage.tsx
+++ src/pages/EditJobPage.tsx
@@ -70,7 +70,7 @@ const EditJobPage = ({ updateJobSubmit }: EditJobPageProps) => {
           <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
             <form onSubmit={submitForm}>
               <h2 className="text-3xl text-center font-semibold mb-6">
-                Add Job
+                Update Job
               </h2>
 
               <div className="mb-4">
@@ -252,7 +252,7 @@ const EditJobPage = ({ updateJobSubmit }: EditJobPageProps) => {
                   className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                   type="submit"
                 >
-                  Add Job
+                  Update Job
                 </button>
               </div>
             </form>
--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -38,7 +38,7 @@ const JobPage = ({ deleteJob }: JobPageProps) => {
 
     <section className="bg-indigo-50">
       <div className="container m-auto py-10 px-6">
-        <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-[70%_28%] w-full gap-6">
           <main>
             <div
               className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
